cda9331e279bc47de458e050224f5fd04a3b4a81
Julien de Carufel <julien.decarufel@rumandcode.io>
2024-06-13 15:41:35 -0400

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PdfGenerator.MixProject do
   def project do
     [
       app: :pdf_generator,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
